8e1421505a8bab89da44988cce020500dd38751c
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-09 13:31:41 +0530

    Error Fix

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import './validators.dart';
 
 class Bloc extends Object with Validators {
-  final _email = StreamController();
-  final _password = StreamController();
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
