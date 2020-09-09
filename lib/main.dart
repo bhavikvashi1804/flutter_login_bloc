25b7dad7479ce7b8dd1fd710a752f6c2c960dfba
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-09 13:55:00 +0530

    Register Provider to the whole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './pages/login_page.dart';
+import './blocs/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+    return Provider(
+      child: MaterialApp(
+        theme: ThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+      ),
     );
   }
 }
