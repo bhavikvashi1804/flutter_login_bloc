d649b825557b4c5ffa6ed3b2b87ba0d7484da723
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-09 12:33:05 +0530

    make the Stream Controller to private so that other programmer does not use it directly

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -1,18 +1,18 @@
 import 'dart:async';
 
 class Bloc {
-  final emailController = StreamController();
-  final passwordController = StreamController();
+  final _emailController = StreamController();
+  final _passwordController = StreamController();
 
   //Add the data to Stream
-  Function(String) get changeEmail => emailController.sink.add;
-  Function(String) get changePassword => passwordController.sink.add;
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
   //this is going to add data into (stream) BLOC server
   //Function String means this function is going to accept String only
 
   //get the data from stream
-  Stream<String> get email => emailController.stream;
-  Stream<String> get password => passwordController.stream;
+  Stream<String> get email => _emailController.stream;
+  Stream<String> get password => _passwordController.stream;
   //this is going to return the data which is present into BLoC (stream)
   /*
     actual code will be like this 
