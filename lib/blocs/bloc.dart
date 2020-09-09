afd96a7e18a6724e021c7b151bd57d65849b2669
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-09 12:50:03 +0530

    apply the validators with tranform method

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -3,19 +3,12 @@ import 'dart:async';
 import './validators.dart';
 
 class Bloc extends Object with Validators {
-  //There are two options
-  //1. extends Object with Validators
-  //Object does not has any method
-  //2. extends Validators
-
   final _email = StreamController();
   final _password = StreamController();
 
-  //Add the data to Stream
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
 
-  //change the data of stream
-  Stream<String> get email => _email.stream;
-  Stream<String> get password => _password.stream;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
 }
