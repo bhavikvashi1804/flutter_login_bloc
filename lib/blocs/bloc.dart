235bc1cbaa441a7fecf6fc7f820185a26bf89e34
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-09 12:29:24 +0530

    access and store data to stream

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -1,3 +1,24 @@
 import 'dart:async';
 
-class Bloc {}
+class Bloc {
+  final emailController = StreamController();
+  final passwordController = StreamController();
+
+  //Add the data to Stream
+  Function(String) get changeEmail => emailController.sink.add;
+  Function(String) get changePassword => passwordController.sink.add;
+  //this is going to add data into (stream) BLOC server
+  //Function String means this function is going to accept String only
+
+  //get the data from stream
+  Stream<String> get email => emailController.stream;
+  Stream<String> get password => passwordController.stream;
+  //this is going to return the data which is present into BLoC (stream)
+  /*
+    actual code will be like this 
+    bloc.email.listen(value){
+      print(value)
+    }
+  */
+
+}
