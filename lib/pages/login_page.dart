9085711354ad58de1ca05b516fd57e8f2faf2ee9
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-09 12:10:02 +0530

    Stateful to Stateless

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../mixins/validation_mixin.dart';
-
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> with ValidationMixin {
-  final _formKey = GlobalKey<FormState>();
-
-  String email = '';
-  String password = '';
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,43 +9,30 @@ class _LoginPageState extends State<LoginPage> with ValidationMixin {
       ),
       body: Container(
         margin: EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email Address',
-                  hintText: 'bhavik@gmail.com',
-                ),
-                validator: validateEmail,
-                onSaved: (value) => email = value,
+        child: Column(
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email Address',
+                hintText: 'bhavik@gmail.com',
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Enter Password',
-                  hintText: 'Password',
-                ),
-                obscureText: true,
-                validator: validatePassword,
-                onSaved: (value) => password = value,
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Password',
+                hintText: 'Password',
               ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    _formKey.currentState.save();
-                    print('Email Address $email');
-                    print('Password $password');
-                  }
-                },
-                child: Text('Login'),
-                color: Colors.blue,
-              ),
-            ],
-          ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20.0),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Login'),
+              color: Colors.blue,
+            ),
+          ],
         ),
       ),
     );
