c28b295921b19fa9625cfc4193293f26f9eb9a91
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-11 08:42:19 +0530

    Create bool stream that can say that form is ready to submit

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
 
 import './validators.dart';
 
@@ -8,6 +10,9 @@ class Bloc extends Object with Validators {
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  Stream<bool> get submitValid =>
+      Rx.combineLatest2(email, password, (e, p) => true);
 
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
